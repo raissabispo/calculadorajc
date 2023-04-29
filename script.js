ec48a205ee01e40e2ddf7920513e181972743fe2
Raissa Vitória <123013855+raissabispo@users.noreply.github.com>
2023-04-29 11:08:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
 
-
-//  function Limpar() {
-//      const total = document.querySelector("input_field")
-//     input_field.value = " ";
-//  }
-  
 document.getElementById('calculate').addEventListener('click', function(){
-    const value = document.getElementById('value').value;
-    const fee = (document.getElementById('fee').value) / 100;
-    const time = document.getElementById('time').value;
 
-//  fórmula do juros compostos :  
-//  M = C.(1 + N) ^ t
-    const total = value * (1 + fee)**time;
+    const value = parseFloat( document.getElementById('value').value);
+    const fee =parseFloat(document.getElementById('fee').value );
+    const valorfinal =parseFloat( document.getElementById('valorfinal').value);
 
-    document.getElementById('total').innerHTML = ("R$ " + total.toFixed(2).replace('.', ','));
+    
+//  fórmula dos juros compostos :  
+//  M = C.(1 + i) ^ t
+
+//   Fórmula para descobrir o tempo com o logaritmo :
+// log M / C = log (1 + i)
+const time = Math.log(valorfinal/value) / Math.log(1+fee);
+
  
+const anos = Math.floor(time);
+const meses = Math.floor((time - anos) * 12);
+const dias = Math.floor(((time - anos) * 12 - meses) * 30);
 
+
+document.getElementById("tempo").innerHTML = "O valor inicial de R$  " + value + " é aplicado a uma taxa de " + fee + "%, gerando um montante de R$ "+ valorfinal+ "<br>"+ ", ao final de um período de: " + "<br>"+"<b>"+ anos  +" ano(os)," + meses +" mes(es) e "+ dias + "  dia(s)." +"<b>";
   
+
 });
-
-
 
     function Limpar()  {
       
           value.value = " ";
           fee.value = " ";
-          time.value = " ";
+         valorfinal.value = " ";
          
-        
+              
     }
     
+   
